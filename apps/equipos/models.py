7945d57907772a23961e57322b1e6fc421d99a1d
Edgard <edgardoherrera165@gmail.com>
2017-04-11 20:13:55 -0600

    error

--- apps/equipos/models.py
+++ apps/equipos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+from apps.ligas.models import Liga
 class Equipos(models.Model):
     nombre = models.CharField(max_length=50)
-    
+    Liga = models.ForeignKey(Liga, null=True, blank=True, on_delete=models.CASCADE)
--- apps/ligas/models.py
+++ apps/ligas/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-from apps.equipos.models import Equipos
+
 
 class Liga(models.Model):
     nombre = models.CharField(max_length=50)
     telefono = models.CharField(max_length=12)
     email = models.EmailField()
-    Equipos = models.ForeignKey(Equipos, null=True, blank=True, on_delete=models.CASCADE)
+    
