85b050c6d2bf6f36269c973700798346103dfda8
Edgard <edgardoherrera165@gmail.com>
2017-04-12 09:05:20 -0600

    creacion de modelos para equipo y liga

--- apps/equipos/admin.py
+++ apps/equipos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from apps.equipos.models import Equipos
 # Register your models here.
+admin.site.register(Equipos)
--- apps/ligas/admin.py
+++ apps/ligas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from apps.ligas.models import Liga
 # Register your models here.
+admin.site.register(Liga)
