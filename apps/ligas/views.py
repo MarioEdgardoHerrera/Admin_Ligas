83ffae602c8f70f0e287e1ce3bc9dd43bd2ec70f
luishck <luisjcenteno@live.com>
2017-04-15 17:36:45 -0600

    Cargar las ligas desde la BD

--- apps/ligas/views.py
+++ apps/ligas/views.py
@@ -3,17 +3,23 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from apps.ligas.forms import LigaForm
+from .models import Liga
 
 # Create your views here.
 def index(request):
-    return render(request, 'liga/index.html' )
+    # Aqui vamos a cargar las ligas que están en la base de datos
+    ligas = Liga.objects.all # Creo que se entiende verdad?
+    return render(request, 'liga/index.html', {'ligas': ligas}) 
+    # Esto es sencillo: Rendereamos el template y de paso le mandamos el objeto con todas las ligas
+
+
 
 def liga_view(request):
     if request.method == 'POST':
         form = LigaForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('liga:index')
+        return redirect('index')
     else: 
         form = LigaForm()
     
