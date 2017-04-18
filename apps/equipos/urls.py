d6698036bf4ad5032be0887665f6778e8c45e1fa
luishck <luisjcenteno@live.com>
2017-04-17 21:18:51 -0600

    Corregir espacios

--- apps/equipos/urls.py
+++ apps/equipos/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index, name='index_equipos'),
-    url(r'^equipos/nuevo/$',views.equipo_view, name='agregar_equipo'),
+    url(r'^$', views.index, name='index_equipos'),
+    url(r'^equipos/nuevo/$', views.equipo_view, name='agregar_equipo'),
 ]
